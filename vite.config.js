1f4c329458eebf37849b07db495e9df9352a2f9d
萤火科技 <635993292@qq.com>
2025-10-22 08:03:04 +0800

    新增：H5端发行后支持独立的config.js文件

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
-import { defineConfig } from "vite";
-import uni from "@dcloudio/vite-plugin-uni";
+import path from 'path'
+import { defineConfig } from 'vite'
+import { multipleEntryFilePlugin } from 'vite-plugin-multiple-entries2'
+import uni from '@dcloudio/vite-plugin-uni'
 
 /**
  * @type {import('vite').UserConfig}
  */
 export default defineConfig({
-  plugins: [uni()],
-});
+  plugins: [
+    uni(),
+    process.env.UNI_PLATFORM === 'h5' ? multipleEntryFilePlugin({
+      chunkName: 'config',
+      entryPath: path.resolve(__dirname, './config.js'),
+      entryFileName: 'config.js',
+      // injectTo: 'body-prepend',
+      crossorigin: true,
+    }) : undefined,
+  ]
+})
